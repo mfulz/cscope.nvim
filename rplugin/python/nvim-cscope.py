75bf0de85c1261bdd3b9a2413ddb0445d969b47f
Matthias Fulz <mfulz@olznet.de>
2016-07-10 16:33:28 +0200

    Initial commit

---
+++ rplugin/python/nvim-cscope.py
@@ -0,0 +1,201 @@
+import neovim
+import os
+import ConfigParser
+import subprocess
+from StringIO import StringIO
+
+
+@neovim.plugin
+class CScope(object):
+
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    def __parse_config(self):
+        self.config = dict()
+
+        config = ConfigParser.ConfigParser()
+        with open(self.project_conf) as stream:
+            stream = StringIO("[root]\n" + stream.read())
+            config.readfp(stream)
+
+        try:
+            self.config['project_name'] = config.get('root', 'name')
+            self.config['project_path'] = os.path.expanduser(
+                config.get('root', 'path'))
+            self.config['file_types'] = config.get('root', 'files')
+        except ConfigParser.NoOptionError as e:
+            return (
+                False,
+                "Error parsing file: '{0}': {1}".format(
+                    self.project_conf,
+                    str(e)))
+
+        if not os.path.exists(self.config['project_path']):
+            return (
+                False, "Project path: '{0}' does not exist".format(
+                    self.config['project_path']))
+        return (True, '')
+
+    def __create_cscope_dir(self):
+        try:
+            if not os.path.isdir(self.cscope_dir):
+                os.mkdir(self.cscope_dir, 0o755)
+            if not os.path.isdir(
+                os.path.join(
+                    self.cscope_dir,
+                    self.config['project_name'])):
+                os.mkdir(
+                    os.path.join(
+                        self.cscope_dir,
+                        self.config['project_name']),
+                    0o755)
+        except OSError as e:
+            return(False, "Couldn't setup cscope directory: '{0}'".format(str(e)))
+        return (True, '')
+
+    def __gen_cscope_files(self):
+        self.cmd_cscope_files = None
+        if self.cscope_ready:
+            cmd = ['find']
+            first = True
+            for file_type in self.config['file_types'].split(','):
+                if first:
+                    cmd.append(self.config['project_path'])
+                    first = False
+                else:
+                    cmd.append('-o')
+                cmd.append('-name')
+                cmd.append(file_type)
+            self.cmd_cscope_files = cmd
+
+    def __update_cscope_files(self):
+        if self.cmd_cscope_files:
+            self.nvim.vars['cscope_debug'] = self.cmd_cscope_files
+            try:
+                with open(os.path.join(self.cscope_dir, self.config['project_name'], 'cscope.files'), 'w+') as cscope_files:
+                    subprocess.call(self.cmd_cscope_files, stdout=cscope_files)
+            except (IOError, subprocess.CalledProcessError) as e:
+                return(False, "Couldn't setup cscope.files: '{0}'".format(str(e)))
+            return(True, '')
+        return(False, "Couldn't setup cscope.files: Command not ready")
+
+    def __update_cscope_out(self):
+        cscope_files = os.path.join(
+            self.cscope_dir,
+            self.config['project_name'],
+            'cscope.files')
+        cscope_out = os.path.join(
+            self.cscope_dir,
+            self.config['project_name'],
+            'cscope.out')
+        if os.path.isfile(cscope_files):
+            try:
+                subprocess.call(['cscope', '-q', '-R', '-b',
+                                 '-i', cscope_files, '-f', cscope_out])
+            except subprocess.CalledProcessError as e:
+                return(False, "Couldn't setup cscope.out: '{0}'".format(str(e)))
+            return(True, '')
+        return(False, "Couldn't setup cscope.out: 'cscope.files' not ready")
+
+    def __init_cscope(self):
+        cscope_out = os.path.join(
+            self.cscope_dir,
+            self.config['project_name'],
+            'cscope.out')
+        if os.path.isfile(cscope_out):
+            if self.cscope_connection_ready:
+                self.nvim.command('cscope reset')
+            else:
+                self.nvim.command('cscope add {0}'.format(cscope_out))
+                self.cscope_connection_ready = True
+
+    @neovim.command('CScopeMapKeys', range='', nargs='*', sync=False)
+    def cscope_map_keys_handler(self, args, range):
+        self.nvim.command(
+            'nmap <unique> <C-\>s :cs find s <C-R>=expand("<cword>")<CR><CR>')
+        self.nvim.command(
+            'nmap <unique> <C-\>g :cs find g <C-R>=expand("<cword>")<CR><CR>')
+        self.nvim.command(
+            'nmap <unique> <C-\>c :cs find c <C-R>=expand("<cword>")<CR><CR>')
+        self.nvim.command(
+            'nmap <unique> <C-\>t :cs find t <C-R>=expand("<cword>")<CR><CR>')
+        self.nvim.command(
+            'nmap <unique> <C-\>e :cs find e <C-R>=expand("<cword>")<CR><CR>')
+        self.nvim.command(
+            'nmap <unique> <C-\>f :cs find f <C-R>=expand("<cfile>")<CR><CR>')
+        self.nvim.command(
+            'nmap <unique> <C-\>i :cs find i ^<C-R>=expand("<cfile>")<CR><CR>')
+        self.nvim.command(
+            'nmap <unique> <C-\>d :cs find d <C-R>=expand("<cword>")<CR><CR>')
+
+    @neovim.command('CScopeUpdate', range='', nargs='*', sync=False)
+    def cscope_update_files_handler(self, args, range):
+        check = self.__update_cscope_files()
+        if not check[0]:
+            self.nvim.command(
+                'echo "Couldn\'t update cscope.files: {0}"'.format(
+                    check[1]))
+
+        check = self.__update_cscope_out()
+        if not check[0]:
+            self.nvim.command(
+                'echo "Couldn\'t update cscope.out: {0}"'.format(
+                    check[1]))
+        self.__init_cscope()
+
+    @neovim.command('CScopeStart', range='', nargs='*', sync=True)
+    def cscope_start_handler(self, args, range):
+        self.cscope_ready = False
+        self.cscope_connection_ready = False
+
+        # setup cscope base dir
+        if not 'cscope_dir' in self.nvim.vars:
+            self.nvim.vars['cscope_dir'] = '~/.cscope'
+        self.cscope_dir = os.path.expanduser(self.nvim.vars['cscope_dir'])
+
+        # setup project configuration
+        if len(args) == 1:
+            if os.path.isfile(args[0]):
+                self.project_conf = os.path.expanduser(args[0])
+            else:
+                self.nvim.command(
+                    'echo "Couldn\'t start CScope: \'{0}\' does not exist."'.format(
+                        args[0]))
+                return
+        else:
+            if os.path.isfile(os.path.join(os.getcwd(), 'cscope.cfg')):
+                self.project_conf = os.path.join(os.getcwd(), 'cscope.cfg')
+            else:
+                self.nvim.command(
+                    'echo "Couldn\'t start CScope: \'cscope.py\' does not exist."')
+                return
+
+        check = self.__parse_config()
+        if not check[0]:
+            self.nvim.command(
+                'echo "Couldn\'t start CScope: {0}"'.format(
+                    check[1]))
+            return
+
+        # create directories for project
+        check = self.__create_cscope_dir()
+        if not check[0]:
+            self.nvim.command(
+                'echo "Couldn\'t start CScope: {0}"'.format(
+                    check[1]))
+            return
+
+        # cscope is ready to process files
+        self.cscope_ready = True
+
+        # generate cscope file
+        self.__gen_cscope_files()
+
+        # initial update
+        if 'cscope_update_on_start' in self.nvim.vars:
+            self.nvim.command('CScopeUpdate')
+
+        # key mapping
+        if 'cscope_map_keys' in self.nvim.vars:
+            self.nvim.command('CScopeMapKeys')
